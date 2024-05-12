4da943a2eb83ee3d7a1fd66b828f89aa4d955026
thiendut0ngu <95538564+thiendut0ngu@users.noreply.github.com>
2024-05-12 11:25:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import torch
 from transformers import BitsAndBytesConfig
 from transformers import pipeline
 import requests
-from flask import Flask
+from flask import Flask, request
 from PIL import Image
 
 quantization_config = BitsAndBytesConfig(
@@ -23,18 +23,68 @@ def home():
 
 @app.route('/process_image', methods=['GET', 'POST'])
 def process_image():
-    image_url = "https://llava-vl.github.io/static/images/view.jpg"
-    image = Image.open(requests.get(image_url, stream=True).raw)
+    #image_url = "https://llava-vl.github.io/static/images/view.jpg"
+    #image = Image.open(requests.get(image_url, stream=True).raw)
+    
+    result = ""
 
+    image = request.get_json()
+    
+    pixel_data = image['pixel_data']
+    width = image['width']
+    height = image['height']
+    
+    tuple_colors = [tuple(color) for color in pixel_data]
+    
+    new_image = Image.new('RGB', (width, height))
+    new_image.putdata(tuple_colors)
+    
+    
+    # Save the new image as a JPEG file
+    new_image.save('reversed_image.jpg', 'JPEG')
+    
+    image_location = "reversed_image.jpg"
+    image = Image.open(image_location)
+
+    #weather
     max_new_tokens = 200
-    prompt = "USER: <image>\nWhat are the things I should be cautious about when I visit this place?\nASSISTANT:"
+    prompt = "USER: <image>\nWhat is the weather type?\nASSISTANT:"
 
     outputs = pipe(image, prompt=prompt, generate_kwargs={"max_new_tokens": 200})
+    index = outputs[0]["generated_text"].index("ASSISTANT:")
+    result = result + "Weather type: " + outputs[0]["generated_text"][index + len("ASSISTANT:"):].strip() + '\n'
+    
+    #Road type
+    prompt = "USER: <image>\nWhat is the road type?\nASSISTANT:"
 
-    # text = "Hello, World!"
+    outputs = pipe(image, prompt=prompt, generate_kwargs={"max_new_tokens": 200})
+    index = outputs[0]["generated_text"].index("ASSISTANT:")
+    result = result + "Road type: " + outputs[0]["generated_text"][index + len("ASSISTANT:"):].strip() + '\n'
+    
+    #Road condition (good or damaged road)
+    prompt = "USER: <image>\nWhat is the road condition(good or damaged road)?\nASSISTANT:"
+
+    outputs = pipe(image, prompt=prompt, generate_kwargs={"max_new_tokens": 200})
+    index = outputs[0]["generated_text"].index("ASSISTANT:")
+    result = result + "Road condition: " + outputs[0]["generated_text"][index + len("ASSISTANT:"):].strip() + '\n'
+
+    #Road condition (good or damaged road)
+    prompt = "USER: <image>\nWhat is the road condition(good or damaged road)?\nASSISTANT:"
+
+    outputs = pipe(image, prompt=prompt, generate_kwargs={"max_new_tokens": 200})
+    index = outputs[0]["generated_text"].index("ASSISTANT:")
+    result = result + "Road condition: " + outputs[0]["generated_text"][index + len("ASSISTANT:"):].strip() + '\n'
 
     # Return the text response
-    return outputs[0]["generated_text"]
+    max_new_tokens = 2000
+    prompt = "USER: <image>\nDescribe in detail each means of transport that caused the accident (color, quantity, ...)\nASSISTANT:"
+    
+    outputs = pipe(image, prompt=prompt, generate_kwargs={"max_new_tokens": max_new_tokens})
+    
+    index = outputs[0]["generated_text"].index("ASSISTANT:")
+    result = result + "In detail each means of transport that caused the accident (color, quantity, ...): " + outputs[0]["generated_text"][index + len("ASSISTANT:"):].strip()
+    
+    return result
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='localhost', port=5050, debug=True, threaded=True)
